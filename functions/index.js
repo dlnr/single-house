7308d4b2fe4d766cb841b3c7215c041578fd3e24
dlnr <niels@dlnr.org>
2019-04-13 17:20:08 +0000

    We have data and caching responses

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,50 @@
 'use strict';
 /**
- * Express style app so I'm going to require Express
+ * Express style app so I'm going to require Express.
+ * Firebase functions to add the SDK.
+ * https is another get method to allow us to keep the connection open
+ * untill we're checked the cache.
+ * axios make it crossbrowser friendly and is a promises based http client.
+ * lru-cache is to set a time limit on cached items.
+ *
  */
 const express = require('express');
 const functions = require('firebase-functions');
+const https = require('https');
+const axios = require('axios');
+const LRUCache = require('lru-cache');
+
+const apiClient = axios.create({
+  baseURL: '',
+  timeout: 10000
+});
+
+const httpsAgent = new https.Agent({
+  keepAlive: true
+});
+
+const lruOptions = {
+  max: 1000,
+  maxAge: 1000 * 60 * 5
+};
+const apiCache = new LRUCache(lruOptions);
+
+async function requestData(url) {
+  const cachedResponse = apiCache.get(url);
+
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+
+  const networkResponse = await apiClient.request({
+    httpsAgent,
+    url
+  });
+
+  const data = networkResponse.data;
+  apiCache.set(url, data);
+  return data;
+}
 
 const app = express();
 
@@ -49,6 +90,16 @@ const footPartial = `
 app.get('/', async(req, res) => {
   res.write(headPartial);
   res.write(`<h1>Single House</h1>`);
+
+  try {
+    const houseData = await requestData(getHouse());
+    res.write(`<strong>${houseData.Plaats}</strong>`);
+  } catch (error) {
+    res.write(`
+    <p>I'm very sorry, demos usually work fine...</p>
+    <pre>${error.message}</pre>
+    `)
+  }
   res.write(footPartial);
   res.end();
 });
